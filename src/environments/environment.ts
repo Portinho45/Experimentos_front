eeb729578a7e5717c503245d78fd1f83f0ab05a2
Portinho45 <xtrevator@gmail.com>
2023-11-21 18:54:45 -0500

    BaseDatos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment={
   production:false,
-  base:"http://localhost:8080"
+  base:"https://appconnection2connection.onrender.com"
   //base:"https://data-connection2connection.onrender.com"
 }
